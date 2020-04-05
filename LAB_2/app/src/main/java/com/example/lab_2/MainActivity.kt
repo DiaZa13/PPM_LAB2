611f088547cc58d1586f7f2108f85967020251c8
DiaZa13 <cor191025@uvg.edu.gt>
2020-04-05 17:00:21 -0600

    extraccion de los datos ingresados por el usuario y calculo del imc

--- LAB_2/app/src/main/java/com/example/lab_2/MainActivity.kt
+++ LAB_2/app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.lab_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
+    var imc : Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var peso = findViewById<View>(R.id.txtpeso) as EditText
+        var estatura = findViewById<View>(R.id.txtestatura) as EditText
+        imc = (peso.text.toString().toDouble()/9.8)/(estatura.text.toString().toDouble()*estatura.text.toString().toDouble())
+        btnCalcular.setOnClickListener(){
+            calcular()
+        }
+
+    }
+
+    private fun calcular() {
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        intent.putExtra("IMC",imc)
+        startActivity(intent)
+        //finish() // finaliza una actividad
     }
 }
