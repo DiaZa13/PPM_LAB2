38e0b9b7e6205e78ddbdac90a74013586e4b4714
DiaZa13 <cor191025@uvg.edu.gt>
2020-04-05 20:17:02 -0600

    Parte logica done

--- LAB_2/app/src/main/java/com/example/lab_2/DisplayMessageActivity.kt
+++ LAB_2/app/src/main/java/com/example/lab_2/DisplayMessageActivity.kt
@@ -2,11 +2,33 @@ package com.example.lab_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
 
+    lateinit var IMCImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        val txtresultado = findViewById<TextView>(R.id.txtMensaje1)
+        var resultado = intent.getStringExtra("IMC")
+        txtresultado.text = resultado
+        var result = resultado.toInt()
+
+        IMCImage = findViewById(R.id.IMC_Image)
+
+        val drawableResource = when (result) {
+            1 -> R.mipmap.desnutricion
+            2 -> R.mipmap.ideal
+            3 -> R.mipmap.sobrepeso
+            else -> R.mipmap.obesidad
+        }
+
+        IMCImage.setImageResource(drawableResource)
+
+
+
     }
 }
--- LAB_2/app/src/main/java/com/example/lab_2/MainActivity.kt
+++ LAB_2/app/src/main/java/com/example/lab_2/MainActivity.kt
@@ -5,29 +5,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    var imc : Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var peso = findViewById<View>(R.id.txtpeso) as EditText
-        var estatura = findViewById<View>(R.id.txtestatura) as EditText
-        imc = (peso.text.toString().toDouble()/9.8)/(estatura.text.toString().toDouble()*estatura.text.toString().toDouble())
+
         btnCalcular.setOnClickListener(){
             calcular()
+        }
+        btnCalcular.setOnLongClickListener(){
+            Toast.makeText(this, "Formula de IMC = peso(kg)/estatura(m)^2", Toast.LENGTH_SHORT).show()
+            true
         }
 
     }
 
     private fun calcular() {
+
+        var peso = txtpeso.text.toString().toDouble()
+        var mass = peso/2.205
+        var estatura = txtestatura.text.toString().toDouble()
+        var height = estatura/100
+        var imc = mass/(height*height)
+
+        var IMC : String = ""
+        if(imc < 18.5){
+            IMC = 1.toString()
+        }else if(imc < 25){
+            IMC = 2.toString()
+        }else if(imc < 30){
+            IMC = 3.toString()
+        }else{
+            IMC = 4.toString()
+        }
+
+
         val intent = Intent(this, DisplayMessageActivity::class.java)
-        intent.putExtra("IMC",imc)
+        intent.putExtra("IMC",IMC)
         startActivity(intent)
         //finish() // finaliza una actividad
     }
+
 }
